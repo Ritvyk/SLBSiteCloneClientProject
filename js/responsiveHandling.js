dca4378651e2f1624ec3627065ae281824a42fd9
Ritvik <ritvyk@gmail.com>
2019-08-21 22:23:48 +0530

    first commit

---
+++ js/responsiveHandling.js
@@ -0,0 +1,31 @@
+function searchblock()
+{
+    document.getElementById("searchInput").style.backgroundColor="white";
+    document.getElementById("searchInput").style.color="black";
+    document.getElementById("searchBtn").style.backgroundColor="black";
+    document.getElementById("searchBtn").style.color="white";
+}
+function dropdown(obj)
+{
+    if(obj.nextElementSibling.style.display==="none")
+    {
+        obj.nextElementSibling.style.display="block";
+    }
+    else{
+        obj.nextElementSibling.style.display="none";
+    }
+}
+function showHamburgerContent(object)
+{
+    if(document.getElementById("hamburger-content").style.display==="none")
+    {
+        object.parentElement.style.backgroundColor="white";
+        object.style.color="black";
+        document.getElementById("hamburger-content").style.display="block";
+    }
+    else{
+        object.parentElement.style.backgroundColor="black";
+        object.style.color="white";
+        document.getElementById("hamburger-content").style.display="none";
+    }
+}
